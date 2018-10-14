91fed08648bd29498d64d4d5ecfbd1867904d5b8
Casey Wang <caseywang@httpeace.com>
2018-10-14 21:50:36 +0800

    improve message sender to avoid empty null value

--- screens/chat/components/room/sender.js
+++ screens/chat/components/room/sender.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Container, Header, Input } from "semantic-ui-react";
+import { Input } from "semantic-ui-react";
 import PropTypes from "prop-types";
+import { isNull, isEmpty, trim } from "lodash";
 
 export default class Sender extends Component {
   static propTypes = {
@@ -11,11 +12,20 @@ export default class Sender extends Component {
   };
   constructor(props) {
     super(props);
+    this.state = {
+      value: null
+    };
     this._input = React.createRef();
   }
   _onSubmit = () => {
     const { onSubmit } = this.props;
-    onSubmit(this._input.current.inputRef.value);
+    onSubmit(value);
+    this._clear();
+  };
+  _clear = () => {
+    this.setState({
+      value: null
+    });
   };
   render() {
     return (
@@ -23,9 +33,16 @@ export default class Sender extends Component {
         ref={this._input}
         placeholder="send something..."
         fluid
+        onChange={({ nativeEvent: { data } }) => {
+          const value = trim(data);
+          this.setState({
+            value: isNull(value) || isEmpty(value) ? null : value
+          });
+        }}
         action={{
           icon: "send",
           color: "teal",
+          disabled: this.state.value ? false : true,
           onClick: this._onSubmit
         }}
       />
