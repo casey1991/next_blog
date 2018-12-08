de1fa547047de1b264337c8e568aa7fd4b9f5815
Casey Wang <caseywang@httpeace.com>
2018-12-08 17:14:41 +0800

    clean up project

--- src/Screens/Home/Components/Main.js
+++ src/Screens/Home/Components/Main.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
-import Radium from "radium";
 class Main extends Component {
   render() {
-    return <div style={[styles.container]} />;
+    return <div />;
   }
 }
-const styles = {
-  container: {
-    padding: "100px 0px",
-    backgroundColor: "#303"
-  }
-};
-export default Radium(Main);
+export default Main;
