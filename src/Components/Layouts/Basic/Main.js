7a5df6fed5ebc04a34853b162b83f6306143a6b0
Casey Wang <caseywang@httpeace.com>
2018-12-10 23:41:29 +0800

    style main content

--- src/Components/Layouts/Basic/Main.js
+++ src/Components/Layouts/Basic/Main.js
@@ -3,6 +3,8 @@ import { compose } from "redux";
 // material ui libs
 import { withStyles } from "@material-ui/core";
 import { Paper, Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import { List, ListSubheader, ListItem, ListItemText } from "@material-ui/core";
 import withWidth, { isWidthUp, isWidthDown } from "@material-ui/core/withWidth";
 class Main extends Component {
   render() {
@@ -10,7 +12,29 @@ class Main extends Component {
     return (
       <div className={classes.root}>
         <div className={classes.content}>
-          <Typography variant="h3">Design</Typography>
+          <Grid container alignItems="center" spacing={24}>
+            <Grid item md={8} sm={12}>
+              <Typography variant="h3" gutterBottom>
+                Design
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                Create intuitive and beautiful products with Material Design.
+              </Typography>
+            </Grid>
+            <Grid item md={4} sm={12}>
+              <List subheader={<Typography gutterBottom>POPULAR</Typography>}>
+                <Typography variant="subtitle1" gutterBottom>
+                  Tools for picking colors
+                </Typography>
+                <Typography variant="subtitle1" gutterBottom>
+                  Iconography
+                </Typography>
+                <Typography variant="subtitle1" gutterBottom>
+                  Text fields
+                </Typography>
+              </List>
+            </Grid>
+          </Grid>
         </div>
       </div>
     );
@@ -18,11 +42,13 @@ class Main extends Component {
 }
 const styles = theme => ({
   root: {
-    width: "100%"
+    width: "100%",
+    backgroundColor: theme.palette.common.white
   },
   content: {
     width: "100%",
     paddingTop: theme.spacing.unit * 15,
+    paddingBottom: theme.spacing.unit * 6,
     [theme.breakpoints.up("lg")]: {
       width: 1280,
       margin: "0 auto",
