3c39acf4e37729e3c8f98922eafbd04187f3e92f
Casey Wang <caseywang@httpeace.com>
2018-12-14 15:24:24 +0800

    calculate offset top

--- src/Components/Editor/index.js
+++ src/Components/Editor/index.js
@@ -7,6 +7,7 @@ import {
   Editor,
   RichUtils
 } from "draft-js";
+import DraftOffsetKey from "draft-js/lib/DraftOffsetKey";
 import Immutable from "immutable";
 // block components
 import { Media, Typography } from "./Components/Block";
@@ -33,13 +34,17 @@ class RichEditor extends Component {
     // focus editor when component did mount
     this._editor.current.focus();
     this.setState({
+      position: {},
       editorState: RichUtils.toggleBlockType(
         this.state.editorState,
         "header-six"
       )
     });
   }
-  _onChange = editorState => this.setState({ editorState });
+  _onChange = editorState => {
+    this.setState({ editorState });
+    this._calculateCurrentOffsetTop();
+  };
   _handleKeyCommand = command => {
     return getDefaultKeyBinding(command);
   };
@@ -51,6 +56,48 @@ class RichEditor extends Component {
         editable: false
       };
     }
+  };
+  _calculateCurrentOffsetTop = () => {
+    const { editorState } = this.state;
+    const selection = editorState.getSelection();
+    if (!selection.getHasFocus()) {
+      this.setState({
+        position: {
+          transform: "scale(0)"
+        }
+      });
+      return;
+    }
+    const currentContent = editorState.getCurrentContent();
+    const currentBlock = currentContent.getBlockForKey(selection.getStartKey());
+    const offsetKey = DraftOffsetKey.encode(currentBlock.getKey(), 0, 0);
+    setTimeout(() => {
+      const node = document.querySelectorAll(
+        `[data-offset-key="${offsetKey}"]`
+      )[0];
+      let editorRoot = this._editor.current.editor;
+      if (!editorRoot) return;
+
+      while (editorRoot.className.indexOf("DraftEditor-root") === -1) {
+        editorRoot = editorRoot.parentNode;
+      }
+
+      const position = {
+        top: node ? node.offsetTop : 0 + editorRoot.offsetTop,
+        transform: "scale(1)",
+        transition: "transform 0.15s cubic-bezier(.3,1.2,.2,1)"
+      };
+      if (this.props.position === "right") {
+        position.left =
+          editorRoot.offsetLeft + editorRoot.offsetWidth + 80 - 36;
+      } else {
+        position.left = editorRoot.offsetLeft - 80;
+      }
+
+      this.setState({
+        position
+      });
+    }, 0);
   };
   render() {
     const { editorState } = this.state;
